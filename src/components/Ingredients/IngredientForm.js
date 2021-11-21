bdea61977bf3c05774747b176fe92d03b276a185
Cate Uch <cat.uch@yandex.ru>
2021-11-21 12:03:56 +0300

    state, useState()

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const [title, setEnteredTitle] = useState('');
+  const [amount, setEnteredAmount] = useState('');
+
+  const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    // setInputState((prevState) => {
+
+    // })
   };
 
   return (
@@ -15,11 +20,26 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(event) => {
+                setEnteredTitle(event.target.value);
+              }}
+            />{" "}
+            {/* [0] - the current state, [1] - function for update */}
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              onChange={(event) => {
+                setEnteredAmount( event.target.value);
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>

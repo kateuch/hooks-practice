28e0c39ced46935a35e665497ee8b515a82dce9e
Cate Uch <cat.uch@yandex.ru>
2021-11-26 02:11:01 +0300

    get request, useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import IngredientList from "./IngredientList";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -6,11 +6,27 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch("https://ingredients-form-default-rtdb.firebaseio.com/ingredients.json").then(response=>response.json())
+      .then(responseData => {
+      const loadedIngredients =[];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      };
+      setUserIngredients(loadedIngredients);
+      })
+    }, [])  //it runs only once, after the first render
+
+
   const addIngredient = (ingredient) => {
-    fetch("https://ingredients-form-default-rtdb.firebaseio.com/", {
+    fetch("https://ingredients-form-default-rtdb.firebaseio.com/ingredients.json", {
       method: "POST",
-      body: JSON.stringify({ ingredient }),
-      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify( ingredient ),
+      headers: { 'Content-Type': 'application/json' }
     })
       .then((response) => {
         return response.json();
